6ef86784714a053cc45f4d38673322d6c4c7e3b8
Dhruv <dhruvpatel9550302@gmail.com>
2025-08-29 15:56:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import os
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
@@ -63,4 +64,4 @@ def recommend():
         return render_template('recommend.html', data=None, error="An error occurred while processing your request.", available_books=available_books)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
